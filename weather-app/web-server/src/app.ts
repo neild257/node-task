9cb586290c1f196d8aee354fcd56acc5cb8b02ef
Harshit Pareek <hp1014@nyu.edu>
2019-10-20 13:56:55 -0700

    feat(): adding the static adding public folder and static servering index.html

--- weather-app/web-server/src/app.ts
+++ weather-app/web-server/src/app.ts
@@ -1,11 +1,27 @@
+import path from 'path';
+
 import express from 'express';
 
-const app  = express();
+const app = express();
 
 const PORT = process.env.PORT || 3000;
+const publicPath = path.join(__dirname, '../public');
+
+app.use(express.static(publicPath));
 
 app.get('/', (req, res) => {
     res.send(`<h2> Node is best in TS</h2>`);
+});
+
+app.get('/weather', (req, res) => {
+    res.send({
+        forcast: 'All clear with no probabilty of rain.',
+        location: 'Los Angeles'
+    });
+});
+
+app.get('/about', (req, res) => {
+    res.send(`<h2> About </h2>`);
 });
 
 app.listen(PORT, () => {
