32a9781b4aa4c8ccea056dc513f7b88969ec9a69
Harshit Pareek <hp1014@nyu.edu>
2019-10-20 12:29:45 -0700

    feat(): adding https core module request

--- weather-app/src/utils/playground.js
+++ weather-app/src/utils/playground.js
@@ -1,31 +1,54 @@
-setTimeout(() => {
-    console.log("Your time is UP");
-}, 2000);
+// setTimeout(() => {
+//     console.log("Your time is UP");
+// }, 2000);
 
 
-const geoCode = (searchString, callback) => {
+// const geoCode = (searchString, callback) => {
 
-    setTimeout(() => {
-        const data = {
-            lat: 0,
-            lon: 0
-        };
+//     setTimeout(() => {
+//         const data = {
+//             lat: 0,
+//             lon: 0
+//         };
 
-        callback(data);
-    }, 2000);
+//         callback(data);
+//     }, 2000);
     
-};
+// };
 
-geoCode('philly', (data) => {
-    console.log("the data is: ", data);
-});
+// geoCode('philly', (data) => {
+//     console.log("the data is: ", data);
+// });
 
-const add = (a, b, callback) => {
-    setTimeout(() => {
-        callback(a+b);
-    }, 2000)
-};
+// const add = (a, b, callback) => {
+//     setTimeout(() => {
+//         callback(a+b);
+//     }, 2000)
+// };
 
-const res = add(4, 5, (sum) => {
-    console.log(sum);
-});
+// const res = add(4, 5, (sum) => {
+//     console.log(sum);
+// });
+
+// Testing the https core module
+const https = require('https');
+const keys = require('../keys');
+
+const url = `https://api.darksky.net/forecast/${keys.mapsDarkSkyAccessToken}/40,-75`; 
+
+https.request(url, (res) => {
+    let data = '';
+    res.on('data', (chunk) => {
+        data = data + chunk.toString();
+    });
+
+    res.on('error', (err) => {
+        console.log(err);
+    });
+
+    res.on('end', () => {
+        const result = JSON.parse(data);
+        console.log(result);
+    });
+}).end();
+
