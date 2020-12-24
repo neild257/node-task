4375bca4ff978fcb1e6b6f99e92c8cf9313149d8
Harshit Pareek <hp1014@nyu.edu>
2020-12-23 22:14:32 -0800

    feat(learning observable

--- tasks/app.ts
+++ tasks/app.ts
@@ -1,1 +1,17 @@
-import { interval } from 'rxjs';
+import { Observable, Subscriber } from 'rxjs';
+
+const subscribe = (subscriber: Subscriber<number>) => {
+    for (let i of [1, 2, 3, 4, 5, 6]) {
+        subscriber.next(i);
+    }
+
+    setTimeout(() => {
+        subscriber.complete();
+    }, 2000);
+
+    return () => console.log('Tear it down');
+};
+
+const source$ = new Observable(subscribe);
+
+source$.subscribe(value => console.log(value));
--- tasks/dist/app.js
+++ tasks/dist/app.js
@@ -1,5 +1,15 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const rxjs_1 = require("rxjs");
-rxjs_1.interval().subscribe((num) => console.log(num));
+const subscribe = (subscriber) => {
+    for (let i of [1, 2, 3, 4, 5, 6]) {
+        subscriber.next(i);
+    }
+    setTimeout(() => {
+        subscriber.complete();
+    }, 2000);
+    return () => console.log('Tear it down');
+};
+const source$ = new rxjs_1.Observable(subscribe);
+source$.subscribe(value => console.log(value), null, () => console.log('complete'));
 //# sourceMappingURL=app.js.map
