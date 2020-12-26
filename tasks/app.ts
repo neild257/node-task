ecb84b8a63a306d13fdcc62a9041b03ea668f145
Harshit Pareek <hp1014@nyu.edu>
2020-12-25 19:11:50 -0800

    feat(learning rxjs)

--- tasks/app.ts
+++ tasks/app.ts
@@ -1,17 +1,36 @@
-import { Observable, Subscriber } from 'rxjs';
+import { Observable, of, from, fromEvent, concat} from 'rxjs';
 
-const subscribe = (subscriber: Subscriber<number>) => {
-    for (let i of [1, 2, 3, 4, 5, 6]) {
-        subscriber.next(i);
-    }
-
-    setTimeout(() => {
-        subscriber.complete();
-    }, 2000);
-
-    return () => console.log('Tear it down');
+const sampleObject = {
+    name: "harshit pareek",
+    age: 29,
+    workplace: 'Extron Electronics',
+    status: 'single'
 };
 
-const source$ = new Observable(subscribe);
 
-source$.subscribe(value => console.log(value));
+const source1$ = from(Object.values(sampleObject));
+
+// source1$.subscribe(value => console.log(value));
+
+const source2$ = from([1, 2, 3, 4]);
+
+// source2$.subscribe(value => console.log(value));
+
+concat(source1$, source2$).subscribe(value => console.log(value));
+
+// Creating first Observable from constructor
+// const subscribe = (subscriber: Subscriber<number>) => {
+//     for (let i of [1, 2, 3, 4, 5, 6]) {
+//         subscriber.next(i);
+//     }
+
+//     setTimeout(() => {
+//         subscriber.complete();
+//     }, 2000);
+
+//     return () => console.log('Tear it down');
+// };
+
+// const source$ = new Observable(subscribe);
+
+// source$.subscribe(value => console.log(value));
--- tasks/dist/app.js
+++ tasks/dist/app.js
@@ -1,15 +1,27 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const rxjs_1 = require("rxjs");
-const subscribe = (subscriber) => {
-    for (let i of [1, 2, 3, 4, 5, 6]) {
-        subscriber.next(i);
-    }
-    setTimeout(() => {
-        subscriber.complete();
-    }, 2000);
-    return () => console.log('Tear it down');
+const sampleObject = {
+    name: "harshit pareek",
+    age: 29,
+    workplace: 'Extron Electronics',
+    status: 'single'
 };
-const source$ = new rxjs_1.Observable(subscribe);
-source$.subscribe(value => console.log(value), null, () => console.log('complete'));
+const source1$ = rxjs_1.from(Object.values(sampleObject));
+// source1$.subscribe(value => console.log(value));
+const source2$ = rxjs_1.from([1, 2, 3, 4]);
+// source2$.subscribe(value => console.log(value));
+rxjs_1.concat(source1$, source2$).subscribe(value => console.log(value));
+// Creating first Observable from constructor
+// const subscribe = (subscriber: Subscriber<number>) => {
+//     for (let i of [1, 2, 3, 4, 5, 6]) {
+//         subscriber.next(i);
+//     }
+//     setTimeout(() => {
+//         subscriber.complete();
+//     }, 2000);
+//     return () => console.log('Tear it down');
+// };
+// const source$ = new Observable(subscribe);
+// source$.subscribe(value => console.log(value));
 //# sourceMappingURL=app.js.map
